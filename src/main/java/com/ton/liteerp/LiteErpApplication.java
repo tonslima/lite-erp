ee3baefbc156603950b8e3920946c324632b87b9
Wellington Lima <wellington.slima@icloud.com>
2026-05-24 00:03:39 -0300

    chore: initial commit

---
+++ src/main/java/com/ton/liteerp/LiteErpApplication.java
@@ -0,0 +1,13 @@
+package com.ton.liteerp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteErpApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LiteErpApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/ton/liteerp/LiteErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ton.liteerp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteErpApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
